e0a437e202baa51bdb832839d85a7ea525fa8b0f
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-04-22 15:32:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,12 +3,12 @@ import os, time
 #from pyrogram.types import BotCommand
 
 class Config:
-    API_ID = int(getenv("API_ID", "23100424"))
-    API_HASH = getenv("API_HASH", "377181507ef31b3ceef9fd6733756feb")
+    API_ID = int(getenv("API_ID", "20652787"))
+    API_HASH = getenv("API_HASH", "5dea928561e4d2eb77a371edf8b2eb2a")
     BOT_TOKEN = getenv("BOT_TOKEN", "7004962339:AAFauWpInLvn1JUjEbTjSnacSQXlnCfRvd8")
     UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "XozMovies")
     UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-2139227512"))
-    ADMIN = list(map(int, getenv("ADMIN", "1058787811").split()))
+    ADMIN = list(map(int, getenv("ADMIN", "1058787811 1242556540").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://evamusicbot:mrwaris04@cluster0.5ad8zuj.mongodb.net/?retryWrites=true&w=majority")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-2132087730"))
     
